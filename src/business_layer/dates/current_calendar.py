68fa1785196ba772bc3e5213475ec11e707541ef
malindev07 <malindev07@yandex.ru>
2025-01-18 00:43:12 +0300

    ADd week day in calendar

--- src/business_layer/dates/current_calendar.py
+++ src/business_layer/dates/current_calendar.py
@@ -3,30 +3,44 @@ import locale
 
 from dataclasses import dataclass, field
 from datetime import datetime
-from weekday import NUM_WEEK_DAY
+from textwrap import indent
+
+from src.business_layer.dates.weekday import NUM_WEEK_DAY
+
+locale.setlocale(locale.LC_ALL, "ru_RU.UTF-8")
 
 
 @dataclass
 class CurrentCalendar:
 
-    month_dates: dict[str, list[list[int]]] = field(default_factory=dict)
+    current_calendar: dict[str, dict[int, str]] = field(default_factory=dict)
     current_year: int = field(default=datetime.now().year)
 
-    def set_month_dates(self):
-        locale.setlocale(locale.LC_ALL, "ru_RU.UTF-8")
+    def set_current_calendar(self):
 
-        for i in range(1, 13):
-            self.month_dates[list(c.month_name)[i]] = c.monthcalendar(
-                self.current_year, i
-            )
+        months = list(c.month_name)
+
+        for num_month in range(1, 13):
+            month = months[num_month]
+            inner_days = c.monthcalendar(self.current_year, num_month)
+
+            for days in inner_days:
+                for day in range(len(days)):
+                    if days[day] != 0:
+
+                        if month not in self.current_calendar:
+                            self.current_calendar[month] = {
+                                days[day]: NUM_WEEK_DAY[day]
+                            }
+                        else:
+                            self.current_calendar[month][days[day]] = NUM_WEEK_DAY[day]
+
+    def get_current_calendar(self):
+        print(self.current_calendar)
+        return self.current_calendar
 
 
-# test = CurrentCalendar()
-# test.set_month_dates()
-#
-# # print(test.month_dates)
-# print(datetime.now().month)
-#
-# for item in test.month_dates["Февраль"]:
-#     for elem in range(len(item)):
-#         print(NUM_WEEK_DAY[elem], item[elem])
+test = CurrentCalendar()
+
+test.set_current_calendar()
+test.get_current_calendar()
