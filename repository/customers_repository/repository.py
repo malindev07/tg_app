9f89057fb0374f3ddecebcc6cd2ac81278340d2d
malindev07 <malindev07@yandex.ru>
2025-05-13 23:47:55 +0300

    feat/customer_feature сделан круд для клиента, добавлена валидация номера телефона

--- repository/customers_repository/repository.py
+++ repository/customers_repository/repository.py
@@ -12,15 +12,15 @@ class CustomerRepository(RepositoryORM):
     session_factory = db_helper.session_factory
 
     async def create(self, model: MODEL) -> MODEL:
-        return await super().create(model = model)
+        return await super().create(model=model)
 
     async def get(self, id_: UUID) -> MODEL:
-        return await super().get(id_ = id_)
+        return await super().get(id_=id_)
 
     async def get_by_field(self, key: str, value: str) -> MODEL | None:
-        return await super().get_by_field(key = key, value = value)
+        return await super().get_by_field(key=key, value=value)
 
     async def delete(self, model: MODEL) -> None:
         return await super().delete(model)
-    
+
     async def update(self) -> MODEL: ...
