f659d7f3eb1dd0cad013d44de1a5488668e0a91e
malindev07 <malindev07@yandex.ru>
2025-05-22 23:00:23 +0300

    feat/staff_feature дабавлены связи записи и персонала

--- repository/records_repository/repository.py
+++ repository/records_repository/repository.py
@@ -17,32 +17,32 @@ class RecordsRepository(RepositoryORM):
     MODEL = RecordModel
     RECORD_STAFF_ASSOCIATION_MODEL = RecordStaffAssociationModel
     session_factory = db_helper.session_factory
-    
+
     async def create(
-            self,
-            model: MODEL,
+        self,
+        model: MODEL,
     ) -> MODEL:
-        record = await super().create(model = model)
+        record = await super().create(model=model)
         return record
-    
+
     async def get(self, id_: UUID) -> MODEL:
-        return await super().get(id_ = id_)
-    
+        return await super().get(id_=id_)
+
     async def get_by_field(self, key: str, value: str) -> MODEL | None:
-        return await super().get_by_field(key = key, value = value)
-    
+        return await super().get_by_field(key=key, value=value)
+
     async def delete(self, model: MODEL) -> None:
         return await super().delete(model)
-    
+
     async def update(self) -> MODEL: ...
-    
+
     async def get_by_date(self, record_date: date) -> Sequence[RecordModel]:
         async with self.session_factory() as session:
             query = select(self.MODEL).where(self.MODEL.record_date == record_date)
             res = await session.execute(query)
             records = res.scalars().all()
             return records
-    
+
     async def create_with_association(self, model: MODEL, staff_id: list[UUID]):
         async with self.session_factory() as session:
             record = await super().create(model)
@@ -51,12 +51,12 @@ class RecordsRepository(RepositoryORM):
             for id_ in staff_id:
                 session.add(
                     self.RECORD_STAFF_ASSOCIATION_MODEL(
-                        record_id = record.id, staff_id = id_
+                        record_id=record.id, staff_id=id_
                     )
                 )
             await session.commit()
             return record
-    
+
     async def get_with_staff(self, record_id: UUID):
         async with self.session_factory() as session:
             result = await session.execute(
--- services/staff_services/converter/converter.py
+++ services/staff_services/converter/converter.py
@@ -7,27 +7,27 @@ from core.db.models import StaffModel
 @dataclass
 class StaffConverter:
     async def schema_to_model(
-            self, schema: StaffSchema | StaffCreateSchema
+        self, schema: StaffSchema | StaffCreateSchema
     ) -> StaffModel:
         return StaffModel(
-            position = schema.position,
-            first_name = schema.first_name,
-            last_name = schema.last_name,
-            middle_name = schema.middle_name,
-            phone = schema.phone,
-            salary_rate = schema.salary_rate,
-            comment = schema.comment,
+            position=schema.position,
+            first_name=schema.first_name,
+            last_name=schema.last_name,
+            middle_name=schema.middle_name,
+            phone=schema.phone,
+            salary_rate=schema.salary_rate,
+            comment=schema.comment,
         )
-    
+
     async def model_to_schema(self, model: StaffModel) -> StaffSchema:
         return StaffSchema(
-            id = model.id,
-            position = model.position,
-            first_name = model.first_name,
-            last_name = model.last_name,
-            middle_name = model.middle_name,
-            phone = model.phone,
-            status = model.status,
-            salary_rate = model.salary_rate,
-            comment = model.comment,
+            id=model.id,
+            position=model.position,
+            first_name=model.first_name,
+            last_name=model.last_name,
+            middle_name=model.middle_name,
+            phone=model.phone,
+            status=model.status,
+            salary_rate=model.salary_rate,
+            comment=model.comment,
         )
