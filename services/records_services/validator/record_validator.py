056ad6ea42115e27d6a97fee6739f47ca2de0b6b
malindev07 <malindev07@yandex.ru>
2025-05-31 00:49:54 +0300

    feat/record_feature валидация записи

--- services/records_services/validator/record_validator.py
+++ services/records_services/validator/record_validator.py
@@ -8,12 +8,12 @@ from api.records.schema.record_schema import RecordWithAssociationSchema
 @dataclass
 class RecordValidator:
     MIN_REPAIR_TIME: timedelta = timedelta(minutes = 30)
-    
+
     def validate_record_time_distance(self, start_time: time, end_time: time) -> bool:
         start_dt = datetime.combine(datetime.today(), start_time)
         end_dt = datetime.combine(datetime.today(), end_time)
         return end_dt - start_dt >= self.MIN_REPAIR_TIME
-    
+
     def validate_record_slot(
             self,
             start_time: time,
