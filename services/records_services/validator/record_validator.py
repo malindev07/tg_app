f93a1d3d65ba3364ddd26bd0978a4a7f1d753d2f
malindev07 <malindev07@yandex.ru>
2025-07-03 23:54:27 +0300

    feat/record_feature фикс багов, редакция проекта

--- services/records_services/validator/record_validator.py
+++ services/records_services/validator/record_validator.py
@@ -17,7 +17,7 @@ class RecordValidator:
 
     @staticmethod
     async def _validate_start_end_time(
-            start_time: datetime, end_time: datetime, workstation: WorkstationSchema
+        start_time: datetime, end_time: datetime, workstation: WorkstationSchema
     ):
         start_dt_ws = datetime.combine(datetime.today(), workstation.start_time)
         end_dt_ws = datetime.combine(datetime.today(), workstation.end_time)
@@ -30,7 +30,7 @@ class RecordValidator:
             return {"end_time": f"Станция заканчивает работу до  {end_time}"}
 
     async def _validate_record_time_distance(
-            self, start_time: datetime, end_time: datetime
+        self, start_time: datetime, end_time: datetime
     ) -> dict[str, str]:
 
         if end_time - start_time >= self.MIN_REPAIR_TIME:
@@ -42,11 +42,11 @@ class RecordValidator:
 
     @staticmethod
     async def _validate_record_staff_slot(
-            start_time: datetime,
-            end_time: datetime,
+        start_time: datetime,
+        end_time: datetime,
         records: Sequence[RecordWithAssociationSchema],
     ) -> dict[UUID, str]:
-        
+
         # Проверка пересечения временных интервалов мастера(если пересекается то {........}, если нет {})
         for record in records:
             start_dt = datetime.combine(datetime.today(), record.start_time)
@@ -61,10 +61,10 @@ class RecordValidator:
 
     async def is_validate(
         self,
-            start_time: datetime,
-            end_time: datetime,
+        start_time: datetime,
+        end_time: datetime,
         records: Sequence[RecordWithAssociationSchema],
-            workstation: WorkstationSchema,
+        workstation: WorkstationSchema,
     ) -> ValidationInfoSchema:
         validate_record_time_distance = await self._validate_record_time_distance(
             start_time, end_time
@@ -73,7 +73,7 @@ class RecordValidator:
             start_time, end_time, records
         )
         validate_st_end_time_ws = await self._validate_start_end_time(
-            start_time = start_time, end_time = end_time, workstation = workstation
+            start_time=start_time, end_time=end_time, workstation=workstation
         )
 
         validation_info = ValidationInfoSchema()
